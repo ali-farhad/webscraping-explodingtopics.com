05d1708492b2558c15dc8a9aeaf1572ced4df14c
ali-farhad <bc170400041@vu.edu.pk>
2020-12-02 00:22:17 +0500

    added ability to add more pages for svrapping

--- index.js
+++ index.js
@@ -4,30 +4,29 @@ const Sheet = require('./sheet');
 
 
 (async function () {
-
-    const res = await fetch('https://explodingtopics.com/topics-this-month');
-    const text = await res.text();
-    const $ = cheerio.load(text);
-
-    const containers = $('.topicInfoContainer').toArray();
-    const trends = containers.map(c => {
-        const active = $(c);
-        const keyword = active.find('.tileKeyword').text();
-        const description = active.find('.tileDescription').text();
-        let searches = active.find('.scoreTagItem').text().split(" ")[0].slice(0, -1);
-        if (searches.length === 0) {
-            searches = "0";
-        }
-
-        return { keyword, description, searches };
-    })
-
-    console.log({ trends });
-
+    let x = 1;
     const sheet = new Sheet();
     await sheet.load();
-    await sheet.addRows(trends)
+
+    while (x <= 3) {
+        const res = await fetch(`https://explodingtopics.com/topics-this-month?page=${x}`);
+        const text = await res.text();
+        const $ = cheerio.load(text);
+
+        const containers = $('.topicInfoContainer').toArray();
+        const trends = containers.map(c => {
+            const active = $(c);
+            const keyword = active.find('.tileKeyword').text();
+            const description = active.find('.tileDescription').text();
+            let searches = active.find('.scoreTagItem').text().split(" ")[0].slice(0, -1);
 
 
+            return { keyword, description, searches };
+        })
+        x++;
+        console.log(x);
+        await sheet.addRows(trends);
+
+    }
 
 })();
